39fd733fd4404f64e399bf84fe44e6fdc62f9607
edmondliusonos <32317817+Emyliu@users.noreply.github.com>
2020-10-15 23:30:04 -0400

    some more stats for nerds

--- script.py
+++ script.py
@@ -6,9 +6,9 @@ import re
 # This script uses the Osu API V1 because the V2 OAuth endpoint is busted...
 
 # Config 
-SECRET_KEY: "ENTER SECRET KEY"
-USERNAME: "ENTER USERNAME"
-NUMBER_OF_PLAYS: 40
+SECRET_KEY = "???"
+USERNAME = "???"
+NUMBER_OF_PLAYS = 50
 
 payload = {
 "k": SECRET_KEY,
@@ -30,28 +30,48 @@ def strip_unicode(string):
 res = requests.get('https://osu.ppy.sh/api/get_user_best', params=payload).json()
 beatmaps = [bmap['beatmap_id'] for bmap in res]
 times = []
+info = []
 
 for idx in range(len(beatmaps)):
-    payload2 = {
-    "k": SECRET_KEY,
-    "b": int(beatmaps[idx])
-    }   
-    timeres = requests.get('https://osu.ppy.sh/api/get_beatmaps', params=payload2)
-    raw_text = timeres.text
-    un_unicoded = strip_unicode(raw_text)
-    times.append(int(json.loads(un_unicoded)[0]['total_length']))
+    try:
+        payload2 = {
+        "k": SECRET_KEY,
+        "b": int(beatmaps[idx])
+        }   
+        timeres = requests.get('https://osu.ppy.sh/api/get_beatmaps', params=payload2)
+        raw_text = timeres.text
+        un_unicoded = strip_unicode(raw_text)
+        jsonified = json.loads(un_unicoded)[0]
+        times.append(int(jsonified['total_length']))
+        info.append((jsonified['title'], jsonified['version']))
+    except:
+        print("Some beatmap is broken")
 
 numerator = 0
 denominator = 0
+minseen = float('inf')
+minseenidx = 0
+maxseen = 0
+maxseenidx = 0
 
 for k in range(len(times)):
+    if times[k] > maxseen:
+        maxseen = times[k]
+        maxseenidx = k
+    if times[k] < minseen:
+        minseen = times[k]
+        minseenidx = k
     multiplier = get_weight(k)
     numerator += times[k] * multiplier
     denominator += multiplier
 
 print("User: {}".format(payload["u"]))
 print("Number of beatmaps analyzed: {}".format(len(times)))
-print("Average time: {}".format(numerator / denominator))
+print("Weighted average time (pp weighting): {}".format(numerator / denominator))
+print("Unweighted average time: {}".format(sum(times) / len(times)))
+print("The longest map in your top {} is {}, {}, and it is {} seconds long".format(len(times), info[maxseenidx][0], info[maxseenidx][1], maxseen))
+print("The shortest map in your top {} is {}, {}, and it is {} seconds long".format(len(times), info[minseenidx][0], info[minseenidx][1], minseen))
+
 
 
 
